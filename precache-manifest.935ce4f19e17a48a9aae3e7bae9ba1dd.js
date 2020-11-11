3750a8e3b0e3584f9d5fcbe94f7dcdf71b4c0c58
Lancaelot <Lancaelot@gmail.com>
2020-11-11 16:45:31 +0200

    Updates

---
+++ precache-manifest.935ce4f19e17a48a9aae3e7bae9ba1dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de640ee7bd7f1c7c57e4a98a8959fa1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42bb096b22d1b7f98871",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "b69c32e4504f074596c3",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "42bb096b22d1b7f98871",
+    "url": "./static/js/2.3b2657ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3b2657ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b69c32e4504f074596c3",
+    "url": "./static/js/main.6f9d3c86.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
